514c98197e56bad84929846d92f48b99239b12dc
adsjinf <jinfrj@hotmail.com>
2024-08-22 12:01:46 -0300

    Implementação de rotinas de encadeamento na interpolação - aula-28

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+interface IPlano {
+  infos: IInfos;
+}
+
+interface IInfos {
+  tipo: string;
+  preco: number;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  //@ts-ignore
+  plano: IPlano = {
+    infos: {
+      tipo: 'simples',
+      preco: 100,
+    },
+  };
+
+    // Exemplo de acesso sem encadeamento opcional
+    obterPreco(): number {
+      // Como o objeto 'plano' e 'infos' sempre são definidos, podemos acessar diretamente
+      return this.plano.infos.preco;
+    }
+  
+    obterTipo(): string {
+      return this.plano.infos.tipo;
+    }
 
 }
