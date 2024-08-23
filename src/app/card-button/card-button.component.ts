eac9e01f7f530e957b99282376b441c65282c435
adsjinf <jinfrj@hotmail.com>
2024-08-22 21:33:36 -0300

    Removendo implementação de templete

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
-  // templateUrl: './card-button.component.html',
-  template: `
-  <div class="card-button">Adquirir</div>
-  `,
+  templateUrl: './card-button.component.html',
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
